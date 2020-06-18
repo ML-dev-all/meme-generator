d43f302c9938468d87810a66aa7343882a064ca5
ML-dev-all <mpdlemos@gmail.com>
2020-06-18 12:11:52 -0500

    Updates

---
+++ precache-manifest.1f44e7f9aac717d4b088de6673365b61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f01627ff34567836bf89695334770c",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "01c8c4c32426858f692b",
+    "url": "/meme-generator/static/css/main.397b860c.chunk.css"
+  },
+  {
+    "revision": "8b0bb354c7f08c47e655",
+    "url": "/meme-generator/static/js/2.4bca04ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.4bca04ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c8c4c32426858f692b",
+    "url": "/meme-generator/static/js/main.26165493.chunk.js"
+  },
+  {
+    "revision": "163fc7ea317d65653242",
+    "url": "/meme-generator/static/js/runtime-main.66d5941a.js"
+  }
+]);
